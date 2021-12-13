2cec433fa45af033e29424a5e8d4fbd88be71f70
Aidin Rezaei <82445380+aidin-rezaei@users.noreply.github.com>
2021-12-14 00:01:39 +0330

    update project

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,7 +1,14 @@
 import React from "react"
-
+import Cards from "../Cards"
+import Footer from "../Footer"
 function Services() {
-    return <h1 className='services'>SERVICES</h1>
+    return (
+        <>
+            <h1 className='services'>SERVICES</h1>
+            <Cards />
+            <Footer />
+        </>
+    )
 
 }
 
--- src/components/pages/SingUp.js
+++ src/components/pages/SingUp.js
@@ -1,7 +1,31 @@
 import React from "react"
+import { Button } from "../Button"
+import Footer from "../Footer"
 
 function SingUp() {
-    return <h1 className='sign-up'>SING UP</h1>
+    return (
+        <>
+            <div className="hero-container2">
+                <video src="/videos/video-1.mp4" autoPlay loop muted />
+                <h1>ADVENTURE AWAITS</h1>
+                <p>What are you waiting for?</p>
+                <div className="hero-btns">
+                    <div className='input-areas2'>
+                        <form>
+                            <input type="email" name='email' placeholder='Your Email' className='footer-input' /><br />
+                            <input type="password" name='password' placeholder='Your Password' className='footer-input' /><br />
+                            <div>
+                                <Button buttonStyle='btn--outline'>Sign Up</Button>
+                                <Button buttonStyle='btn--primary'>Log in</Button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
 
 }
 
